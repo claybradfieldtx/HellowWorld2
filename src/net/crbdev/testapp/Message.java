701234b680c05cdd33373d9b548e1f3156f32efe
ClayBradfield <github@claybradfield.com>
2017-09-12 23:21:56 -0500

    Now 102

--- src/net/crbdev/testapp/Message.java
+++ src/net/crbdev/testapp/Message.java
@@ -2,7 +2,7 @@ package net.crbdev.testapp;
 
 public class Message 
 {
-	String message = "Hello World! (101) \n";
+	String message = "Hello World! (102) \n";
 	
 	public String getMessage() {
 		return message;
