274b764a8ebccef85ef5c363283edf13df2a6884
ClayBradfield <github@claybradfield.com>
2017-09-12 21:19:27 -0500

    Putting it out there

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,9 @@ public class Hello {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!!\n");
+		Message m = new Message();
+		
+		System.out.println(m.getMessage());
 	}
 
 }
