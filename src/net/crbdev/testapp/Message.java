72c85671f8ceb0f282fc07916a20f45c9af6a4e8
ClayBradfield <github@claybradfield.com>
2017-09-13 21:08:48 -0500

    Try number 105

--- src/net/crbdev/testapp/Message.java
+++ src/net/crbdev/testapp/Message.java
@@ -2,7 +2,7 @@ package net.crbdev.testapp;
 
 public class Message 
 {
-	String message = "Hello World! (102) \n";
+	String message = "Hello World! (105) \n";
 	
 	public String getMessage() {
 		return message;
